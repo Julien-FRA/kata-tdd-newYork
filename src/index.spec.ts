88395eb9c474fb03528b2c4c871c72123402d8d6
Julien <jujufrancois1999@gmail.com>
2022-05-30 12:32:34 +0200

    Refacto, end of kata

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,50 +8,71 @@ it("That's a test!", function () {
 });
 
 it("Should calculate range on the same coord", function () {
+  // Given
   const startCoord = { x: 0, y: 0 };
   const finalCoord = { x: 0, y: 0 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
+  // Then
   expect(calculPath).toEqual(0);
 });
 
 it("Should calculate range with only one move Y", function () {
+  // Given
   const startCoord = { x: 0, y: 0 };
   const finalCoord = { x: 0, y: 1 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
+  // Then
   expect(calculPath).toEqual(1);
 });
 
 it("Should calculate range with only two move Y", function () {
+  // Given
   const startCoord = { x: 0, y: 0 };
   const finalCoord = { x: 0, y: 2 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
+  // Then
   expect(calculPath).toEqual(2);
 });
 
 it("Should calculate range with starter different than 0", function () {
+  // Given
   const startCoord = { x: 0, y: 1 };
   const finalCoord = { x: 0, y: 2 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
+  // Then
   expect(calculPath).toEqual(1);
 });
 
 it("Should calculate range with starter coords > final coords", function () {
+  // Given
   const startCoord = { x: 0, y: 3 };
   const finalCoord = { x: 0, y: 2 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
+  // Then
   expect(calculPath).toEqual(1);
 });
 
 it("Should calculate range with starter coords negative", function () {
+  // Given
   const startCoord = { x: 0, y: -1 };
   const finalCoord = { x: 0, y: 2 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
-  expect(calculPath).toEqual(1);
+  // Then
+  expect(calculPath).toEqual(3);
 });
 
 it("Should calculate range with starter and final coords negative", function () {
+  // Given
   const startCoord = { x: 0, y: -1 };
   const finalCoord = { x: 0, y: -2 };
+  // When
   const calculPath = calcRange(startCoord, finalCoord);
+  // Then
   expect(calculPath).toEqual(1);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export const calcRange = (startCoord, finalCoord) => {
-    if ((finalCoord.x >= 0) && (finalCoord.y >= 0) && (startCoord.x >= 0) && (startCoord.y >= 0)) {
-        return Math.abs(finalCoord.y - startCoord.y);
-    }
-    if ((finalCoord.x <= 0) && (finalCoord.y <= 0) && (startCoord.x <= 0) && (startCoord.y <= 0)) {
-        return Math.abs(startCoord.y - finalCoord.y);
-    }
-    return startCoord.y + finalCoord.y;
+    return Math.abs(finalCoord.y - startCoord.y) + Math.abs(finalCoord.x - startCoord.x);
 }
